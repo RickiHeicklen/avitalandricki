725b048f785a1abf0cb0eaeb5197a93c374d4c6d
Ricki Heicklen <ricki.heicklen@gmail.com>
2024-04-12 14:09:04 -0400

    adding a whole bunch of pages

---
+++ loadHeader.js
@@ -0,0 +1,9 @@
+// loadHeader.js
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('header.html')
+        .then(response => response.text())
+        .then(html => {
+            document.getElementById('header-nav').innerHTML = html;
+        })
+        .catch(err => console.error('Failed to load header:', err));
+});
